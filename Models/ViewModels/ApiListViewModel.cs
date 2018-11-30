090aa89366ea8d77d1b7138d88bbbfbe1a2013ff
Jeff Grissom <jgrissom@stf044941.local>
2018-11-30 16:09:33 -0600

    Refactor ApiListViewModel
    Shorten the JSON property names

--- Models/ViewModels/ApiListViewModel.cs
+++ Models/ViewModels/ApiListViewModel.cs
@@ -11,9 +11,13 @@ namespace Modas.Models.ViewModels
     }
     public class ApiViewEvent
     {
+        [JsonProperty(PropertyName = "id")]
         public int EventId { get; set; }
+        [JsonProperty(PropertyName = "ts")]
         public DateTime TimeStamp { get; set; }
+        [JsonProperty(PropertyName = "flag")]
         public bool Flagged { get; set; }
+        [JsonProperty(PropertyName = "loc")]
         public string LocationName { get; set; }
     }
 }
